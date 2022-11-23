44b120cc41a7f46a728899fe5308c71b8e37b88e
Wendy Randolph <wendy.randolph5@gmail.com>
2022-11-22 20:46:41 -0500

    Use def-new to create new app template

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to the-dress-code/task-timer
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/the_dress_code/task_timer_test.clj
@@ -0,0 +1,7 @@
+(ns the-dress-code.task-timer-test
+  (:require [clojure.test :refer :all]
+            [the-dress-code.task-timer :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
